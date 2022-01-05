3d1b1ccd91c714aba69a2e363cd7e20ae5526c15
Karim Daw <karimdaw91@gmail.com>
2022-01-05 21:49:11 +0100

    add comment for dev server, and change naming for best practises

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 
+# use uvicorn main:app to start production server
+# use uvicorn main:app --reload to start development server
+
 app = FastAPI()
 
 class Post(BaseModel):
@@ -20,10 +23,10 @@ async def root():
 def get_posts():
     return {"data": "This is your post"}
 
-@app.post("/createposts")
+@app.post("/posts")
 def get_create_ports(new_post: Post):
     print(new_post)
     print(new_post.dict())
-    return {"data": "new post"}
+    return {"data": "this is your post"}
 
 # title str, content str
