b87aee06981efd4df1c46657faa9a941c8db81b2
Karim Daw <karimdaw91@gmail.com>
2022-01-06 23:44:57 +0100

    add 404 exception for delete

--- main.py
+++ main.py
@@ -67,5 +67,8 @@ def delete_post(id: int):
     # delete post
     # find the index in the array that has required ID
     index = find_index_post(id)
+    if index == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist")
     my_posts.pop(index)
-    return {'message': "post was succussfully deleted"}
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
