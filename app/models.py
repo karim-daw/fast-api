7e32895c2c6efcc3d210cf65f504a26b1fb6eaea
Karim Daw <karimdaw91@gmail.com>
2022-01-11 23:54:15 +0100

    debug connection to db with sqlalchemy

--- app/models.py
+++ app/models.py
@@ -3,11 +3,12 @@ from sqlalchemy import Column, Integer, String, Boolean
 
 class Post(Base):
     # table name
-    __tablename__ = "post"
+    __tablename__ = "posts"
 
     # columns
     id = Column(Integer, primary_key=True, nullable=False)
-    title = Column(String, primary_key=False)
+    title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_defualt='TRUE',nullable=False)
+
 
