e92a34fb3105fd9ed96c6695ec0b0a2f68869dcd
karimdaw1991 <karimdaw91@gmail.com>
2022-01-20 23:38:33 +0100

    add user authorization for endppints@

--- app/routers/post.py
+++ app/routers/post.py
@@ -13,7 +13,8 @@ router = APIRouter(
 
 # get all posts
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db),
+    user_id: int = Depends(oauth2.get_current_user)):
 
     posts = db.query(models.Post).all()
 
@@ -24,7 +25,7 @@ def get_posts(db: Session = Depends(get_db)):
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate,
     db: Session = Depends(get_db),
-    user_id: int = Depends(oauth2.get_current_user)): # dependancy that forces users to be logged in
+    user_id: int = Depends(oauth2.get_current_user)): # dependancy forces logged in
 
     new_post = models.Post(**post.dict())
     
@@ -39,7 +40,8 @@ def create_posts(post: schemas.PostCreate,
 
 # getting singular post
 @router.get("/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
+def get_post(id: int, db: Session = Depends(get_db),
+    user_id: int = Depends(oauth2.get_current_user)):
     
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
@@ -50,13 +52,15 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 # deleting a post
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(id: int, db: Session = Depends(get_db),
+    user_id: int = Depends(oauth2.get_current_user)):
 
     post = db.query(models.Post).filter(models.Post.id == id)
 
     if post.first() == None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist")
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id:{id} does not exist")
     # worth reading about in doc under "session basics"
     post.delete(synchronize_session=False)
     db.commit()
@@ -65,14 +69,17 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 # updating a post
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.PostCreate,
+    db: Session = Depends(get_db),
+    user_id: int = Depends(oauth2.get_current_user)):
 
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
 
     if post == None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist")
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id:{id} does not exist")
 
     # if post exists, use update() to update with post from user 
     post_query.update(updated_post.dict(), synchronize_session=False)
