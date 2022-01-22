5c69d2c566cbebe16bed9d71b13a803d1c6712a6
karimdaw1991 <karimdaw91@gmail.com>
2022-01-23 00:14:03 +0100

    add relationship to User from posts for json response

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from sqlalchemy.sql.expression import text, true
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 from sqlalchemy import Column, ForeignKey, ForeignKeyConstraint, Integer, String, Boolean
+from sqlalchemy.orm import relationship
 
 # this is sqlalchemy model
 
@@ -15,6 +16,10 @@ class Post(Base):
     published = Column(Boolean, server_default='TRUE',nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
     owner_id = Column(Integer, ForeignKey("users.id",ondelete=CASCADE), nullable=False)
+    
+    # create a owner property that creates a relationship to User class
+    owner = relationship("User")
+
 
 class User(Base):
     __tablename__ = "users"
@@ -23,6 +28,7 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
 
 
 
--- app/schemas.py
+++ app/schemas.py
@@ -2,27 +2,9 @@ from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
 
+
 # Pydantic model, it will ensure that the data is valid for the schema
 # you can create differnet mdoels for different requests so that for more flexibility
-
-class PostBase(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
-class PostCreate(PostBase):
-    pass
-
-class Post(PostBase):
-    id: int
-    created_at: datetime
-    owner_id: int
-
-    # tells pydantic to ignore the fact that it isnt a dict
-    class Config:
-        orm_mode = True
-
-
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
@@ -39,6 +21,30 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+class PostCreate(PostBase):
+    pass
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+
+    # return pydantic model called UserOut to complete db relationship
+    owner: UserOut
+
+    # tells pydantic to ignore the fact that it isnt a dict
+    class Config:
+        orm_mode = True
+
+
+
 
 class Token(BaseModel):
     access_token: str
