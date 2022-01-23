32b56c536a0617901479255abb7fb8d5853ea548
karimdaw1991 <karimdaw91@gmail.com>
2022-01-23 20:46:32 +0100

    add skip and limit to query params

--- app/routers/post.py
+++ app/routers/post.py
@@ -14,10 +14,14 @@ router = APIRouter(
 # get all posts
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db),
-    current_user: int = Depends(oauth2.get_current_user)):
+    current_user: int = Depends(oauth2.get_current_user),
+    limit: int = 10,
+    skip: int = 0):
+
+    print(limit)
 
     # only retrieve posts if it comes for current user
-    posts = db.query(models.Post).all()
+    posts = db.query(models.Post).limit(limit).offset(skip).all()
 
     #print(posts)
     return posts
