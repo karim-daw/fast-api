a5bc350896393c814f76307ec3cd49c6d497f0e9
karimdaw1991 <karimdaw91@gmail.com>
2022-01-30 23:31:23 +0100

    fix cascade

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE',nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    owner_id = Column(Integer, ForeignKey("users.id",ondelete=CASCADE), nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id",ondelete="CASCADE"), nullable=False)
     
     # create a owner property that creates a relationship to User class
     owner = relationship("User")
